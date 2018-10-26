ab2654eeac5ed305a841e964f1613996e23e21ca
Bo <ganbo.me@gmail.com>
2018-10-26 11:13:12 +1100

    longest word chanllenge

---
+++ src/longestWord.js
@@ -0,0 +1,16 @@
+/*
+Challenge
+Using the JavaScript language, have the function LongestWord(sen) take the sen parameter being passed 
+and return the largest word in the string. If there are two or more words that are the same length, 
+return the first word from the string with that length. 
+Ignore punctuation and assume sen will not be empty. 
+
+*/
+
+(function (exports) {
+
+  exports.LongestWord = function (sen) {
+
+    return sen;
+  };
+}((typeof window === 'undefined') ? module.exports : window));
